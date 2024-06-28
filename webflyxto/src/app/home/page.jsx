2abcb617c52bad3b167932271f8456d0e3486b25
YasanthFlyxto <143312439+YasanthFlyxto@users.noreply.github.com>
2024-06-28 21:28:07 +0530

    techstack

--- webflyxto/src/app/home/page.jsx
+++ webflyxto/src/app/home/page.jsx
@@ -1,12 +1,14 @@
 import Header from "./header";
 import Container from '@/layout/container';
 import WhatWeDo from "./WhatWeDo";
+import Techstack from "./techstack";
 
 const Home = () => {
   return (
     <Container>
       <Header/>
       <WhatWeDo/>
+      <Techstack/>
     </Container>
   );
 }
---
+++ webflyxto/src/app/home/techstack.jsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+import "./techstack.css";
+import ReactLogo from '../../assets/reactlogo.svg';  // Update the path to your actual logo files
+
+const Techstack = () => {
+  return (
+    <div className=" grid md:grid-cols-2 grid-cols-1 md:py-40">
+      <div className=' flex justify-center'>
+        <div className="circle2 md:scale-100 scale-75">
+          <div className='ray4'></div>
+          <div className="tech-icon react techlogos">
+            <div className='ray3'></div>
+            <Image src={ReactLogo} alt="React" width={100} height={100} />
+          </div>
+          <div className="tech-icon tailwind techlogos  ">
+            <div className='ray3'></div>
+            <Image src={ReactLogo} alt="Tailwind" width={100} height={100} />
+
+          </div>
+          <div className="tech-icon wordpress techlogos ">
+            <div className='ray3'></div>
+            <Image src={ReactLogo} alt="WordPress" width={100} height={100} />
+          </div>
+          <div className="tech-icon mysql techlogos">
+            <div className='ray3'></div>
+            <Image src={ReactLogo} alt="MySQL" width={100} height={100} />
+          </div>
+          <div className="tech-icon aws techlogos">
+            <div className='ray3'></div>
+            <Image src={ReactLogo} alt="AWS" width={100} height={100} />
+          </div>
+          <div className="tech-icon laravel techlogos">
+            <div className='ray3'></div>
+            <Image src={ReactLogo} alt="Laravel" width={100} height={100} />
+          </div>
+          <div className="tech-icon middleIcon flex items-center justify-center ">
+
+            <p className='text-xl text-center scale-150'>Experts In</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p className='text-4xl  text-center pt-8'>Modern Tech Era</p>
+        <p className=' text-justify md:text-xl pt-8 px-4'>At Flyxto Technologies, we embrace the modern tech era by leveraging cutting-edge technologies to deliver innovative solutions across web, mobile, and digital platforms, empowering businesses to succeed in a dynamic digital landscape.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Techstack;
