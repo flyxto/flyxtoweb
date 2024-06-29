2c74f018e7e3edfd5165aad0d0be748e3c8d88f8
YasanthFlyxto <143312439+YasanthFlyxto@users.noreply.github.com>
2024-06-29 17:18:48 +0530

    projectsUpdate

--- webflyxto/src/app/home/cards/projectcard.jsx
+++ webflyxto/src/app/home/cards/projectcard.jsx
@@ -1,30 +1,34 @@
 import Image from 'next/image';
 import '../projects.css';
 
-const ProjectCard = ({ imageSrc, title, description, tags, projectTypes }) => {
+const ProjectCard = ({ imageSrc, title, description, tags, projectTypes, link }) => {
   return (
     <article className="card5">
       <div className='card-content'>
-        <div className="card-img relative">
-          <div className="project-hover absolute flex justify-center items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" width="4em" height="4em" color="black" strokeLinejoin="round" strokeLinecap="round" viewBox="0 0 24 24" strokeWidth="1" fill="none" stroke="#ffffff">
-              <line y2="12" x2="19" y1="12" x1="5"></line>
-              <polyline points="12 5 19 12 12 19"></polyline>
-            </svg>
+        <a href={link}>
+          <div className="card-img relative">
+            <div className="project-hover absolute flex justify-center items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" width="4em" height="4em" color="black" strokeLinejoin="round" strokeLinecap="round" viewBox="0 0 24 24" strokeWidth="1" fill="none" stroke="#ffffff">
+                <line y2="12" x2="19" y1="12" x1="5"></line>
+                <polyline points="12 5 19 12 12 19"></polyline>
+              </svg>
+            </div>
+            <div className="card-imgs pv overflow-hidden">
+              <Image src={imageSrc} width={310} height={100} alt='' />
+            </div>
           </div>
-          <div className="card-imgs pv overflow-hidden">
-            <Image src={imageSrc} width={310} height={100} alt='' />
-          </div>
-        </div>
+        </a>
 
-        <div className="project-info">
-          <div className="flex-col">
-            <div className="project-title">{title}</div>
-            <span className="tag">Web Application</span>
+        <div className="project-info grid grid-rows-2">
+          <div className="h-40">
+            <div className="flex-col">
+              <div className="project-title">{title}</div>
+              <span className="tag">Web Application</span>
+            </div>
+            <span className="lighter text-justify">
+              {description}
+            </span>
           </div>
-          <span className="lighter text-justify">
-            {description}
-          </span>
           <div className="types pt-2">
             {projectTypes.map((type, index) => (
               <span key={index} className="project-type">#{type}</span>
--- webflyxto/src/app/home/projects.jsx
+++ webflyxto/src/app/home/projects.jsx
@@ -1,7 +1,6 @@
 import ProjectCard from './cards/projectcard';
 import MetaXPre from "../../assets/metaXPrev.png";
 
-
 const Projects = () => {
   const projects = [
     {
@@ -9,33 +8,37 @@ const Projects = () => {
       title: "MetaX Global",
       description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae voluptas ullam aut incidunt minima.",
       tags: ["Web Application"],
-      projectTypes: ["modern", "dashboards"]
+      projectTypes: ["modern", "dashboards"],
+      link: "#",
     },
     {
       imageSrc: MetaXPre,
       title: "Another Project",
       description: "Another project description goes here. It is also a web application.",
       tags: ["Web Application"],
-      projectTypes: ["innovative", "responsive"]
+      projectTypes: ["innovative", "responsive"],
+      link: "#",
     },
     {
       imageSrc: MetaXPre,
-      title: "Another Project",
-      description: "Another project description goes here. It is also a web application.",
+      title: "Third Project",
+      description: "Description for the third project.",
       tags: ["Web Application"],
-      projectTypes: ["innovative", "responsive"]
+      projectTypes: ["creative", "design"],
+      link: "#",
     },
     {
       imageSrc: MetaXPre,
-      title: "Another Project",
-      description: "Another project description goes here. It is also a web application.",
+      title: "Fourth Project",
+      description: "Description for the fourth project.",
       tags: ["Web Application"],
-      projectTypes: ["innovative", "responsive"]
+      projectTypes: ["fast", "modern"],
+      link: "#",
     },
   ];
 
   return (
-    <div className=''>
+    <div>
       <p className='text-center gradient-text text-4xl mb-8 font-semibold'>Case Studies</p>
       <div className="projects-container">
         {projects.map((project, index) => (
