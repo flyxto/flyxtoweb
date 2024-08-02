cbfccc45c277f382b9da42cf2acce727a97eec4b
YasanthFlyxto <143312439+YasanthFlyxto@users.noreply.github.com>
2024-08-02 17:07:00 +0530

    navbar update

--- webflyxto/src/app/layout.jsx
+++ webflyxto/src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
+import Navbar2 from "./components/navbar2";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
+        {/* <Navbar/> */}
+        <Navbar2/>
         {children}</body>
     </html>
   );
