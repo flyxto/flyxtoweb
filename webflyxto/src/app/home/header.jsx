bb14ad5a4d2b08442a0c81f61b3345bc25d7fc2c
YasanthFlyxto <143312439+YasanthFlyxto@users.noreply.github.com>
2024-08-04 20:17:49 +0530

    animation update

--- webflyxto/src/app/home/header.jsx
+++ webflyxto/src/app/home/header.jsx
@@ -1,71 +1,95 @@
-"use client"
+"use client";
+import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
-import '@/app/home/header.css';
+import '@/app/home/header.css'; // Ensure this file includes the blur effect CSS
 import Container from '@/layout/container';
 import LogoSlider from './brands';
 
 const Header = () => {
+  const [scrollY, setScrollY] = useState(0);
+
+  const handleScroll = () => {
+    setScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  // Calculate blur and opacity based on scrollY
+  const blurAmount = Math.min(scrollY / 50, 10); // Adjust divisor to control blur speed
+  const opacityAmount = Math.max(1 - scrollY / 1500, 0); // Adjust divisor to control opacity
+
   return (
     <Container>
-      <div>
-        <div className="ray"></div>
-        <div className='md:mt-56 mt-40'>
-          <div>
-            <div className='h-14 absolute shine'></div>
-            <motion.div
-              className="gradient-text md:text-6xl font-medium text-3xl text-center"
-              initial={{ opacity: 0, y: -50 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 1 }}
-            >
-              Build Software To Make Things Easier.
-            </motion.div>
-          </div>
-        </div>
+      <div
+        className="header-wrapper"
+        style={{ filter: `blur(${blurAmount}px)`, opacity: opacityAmount }}
+      >
         <div>
-          <motion.p
-            className="gradient-text md:text-[20px] text-[16px] text-center mt-[25px] prevent-select"
-            initial={{ opacity: 0, y: 30 }}
+          <div
+
+            className="ray"
+          ></div>
+          <motion.div
+            className='md:mt-56 mt-40'
+            initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 0.5 }}
+            transition={{ duration: 1, delay: 0.7 }}
           >
-            Partner with our visionary team of experts to transform your <br />digital product dreams into reality.
-          </motion.p>
-        </div>
-        <div className='flex justify-center md:pt-20 pt-16'>
-          <motion.button
-            className="button md:scale-100 scale-90"
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 0.5 }}
-          >
-            <div className="blob1"></div>
-            <div className="blob2"></div>
-            <div className="inner ">
-              <p className="px-4 text-lg">Schedule a Meeting</p>
+            <div>
+             
+              <motion.div
+                className="gradient-text md:text-6xl font-medium text-3xl text-center"
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1, delay: 0.9 }}
+              >
+                Build Software To Make Things Easier.
+              </motion.div>
             </div>
-          </motion.button>
-        </div>
-        <div className="md:pt-36 pt-20">
-          <motion.p
-            className='text-center text-lg gradient-text mb-4'
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1, delay: 1 }}
-          >
-            Trusted By
-          </motion.p>
+          </motion.div> <div className='h-14 absolute shine'></div>
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 1.1 }}
+          >
+            <p className="gradient-text md:text-[20px] text-[16px] text-center mt-[25px] prevent-select">
+              Partner with our visionary team of experts to transform your <br />digital product dreams into reality.
+            </p>
+          </motion.div>
+          <motion.div
+            className='flex justify-center md:pt-20 pt-16'
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 1, delay: 1.3 }}
+          >
+            <button className="button md:scale-100 scale-90">
+              <div className="blob1"></div>
+              <div className="blob2"></div>
+              <div className="inner ">
+                <p className="px-4 text-lg">Schedule a Meeting</p>
+              </div>
+            </button>
+          </motion.div>
+          <motion.div
+            className="md:pt-36 pt-20"
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1, delay: 1.5 }}
           >
-            <LogoSlider />
+            <p className='text-center text-lg gradient-text mb-4'>
+              Trusted By
+            </p>
+            <div>
+              <LogoSlider />
+            </div>
           </motion.div>
         </div>
       </div>
     </Container>
   );
-}
+};
 
 export default Header;
---
+++ webflyxto/src/app/home/whyus.jsx
@@ -0,0 +1,69 @@
+"use client";
+import { useEffect, useRef, useState } from 'react';
+import { motion } from 'framer-motion';
+
+const WhyUs = () => {
+  const [isInView, setIsInView] = useState(false);
+  const [hasAnimated, setHasAnimated] = useState(false);
+  const refs = useRef([]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsInView(true);
+          if (!hasAnimated) {
+            setHasAnimated(true);
+          }
+        }
+      },
+      { threshold: 0.5 } // Adjust threshold as needed
+    );
+
+    refs.current.forEach(ref => {
+      if (ref) observer.observe(ref);
+    });
+
+    return () => {
+      refs.current.forEach(ref => {
+        if (ref) observer.unobserve(ref);
+      });
+    };
+  }, [hasAnimated]);
+
+  return (
+    <div>
+      <div className="h-screen flex-col items-center justify-center ">
+        <motion.p
+          ref={el => refs.current[0] = el}
+          className="text-center text-7xl font-thin gradient-text"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: isInView && hasAnimated ? 1 : 0, y: isInView && hasAnimated ? 0 : 50 }}
+          transition={{ duration: 1, delay: 0.1 }}
+        >
+          Why Us ?
+        </motion.p>
+        <motion.p
+          ref={el => refs.current[1] = el}
+          className="text-center text-7xl mt-8 gradient-text"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: isInView && hasAnimated ? 1 : 0, y: isInView && hasAnimated ? 0 : 50 }}
+          transition={{ duration: 1, delay: 0.5}}
+        >
+          Because We Listen First
+        </motion.p>
+        <motion.p
+          ref={el => refs.current[2] = el}
+          className="text-center text-8xl font-bold mt-4 gradient-text"
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: isInView && hasAnimated ? 1 : 0, y: isInView && hasAnimated ? 0 : 50 }}
+          transition={{ duration: 1, delay: 0.7}}
+        >
+          Then Solve
+        </motion.p>
+      </div>
+    </div>
+  );
+};
+
+export default WhyUs;
