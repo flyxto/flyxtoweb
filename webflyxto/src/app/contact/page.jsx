683f92c357531f7e2c73aed6876ff5765d077e2e
YasanthFlyxto <143312439+YasanthFlyxto@users.noreply.github.com>
2024-08-07 15:10:37 +0530

    update contact

--- webflyxto/src/app/contact/page.jsx
+++ webflyxto/src/app/contact/page.jsx
@@ -61,7 +61,7 @@ const Contact = () => {
       >
         <Image
           src={banner}
-          className='brightness-[80%]'
+          className='brightness-[60%]'
           objectFit='cover'
           alt='Background Banner'
         />
@@ -80,7 +80,7 @@ const Contact = () => {
               transition={{ duration: 1, delay: 0.5 }} // Add a delay here
             >
               <p className='text-5xl font-semibold gradient-text text-center'>Get in touch</p>
-              <div className='w-96 p-8 mt-3 bg-black/50 backdrop-blur-sm rounded-lg shadow-lg'>
+              <div className='w-96 p-8 mt-3 bg-black/40 backdrop-blur-sm rounded-3xl rounded-tr-[100px] rounded-bl-[100px] shadow-lg'>
                 <form onSubmit={handleSubmit}>
                   <div className='mt-4'>
                     <label htmlFor="name" className='block text-sm font-medium '>Your Name :</label>
